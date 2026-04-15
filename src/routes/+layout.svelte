a04c245c1dffba982ac2a3e55d07d5137ae4e171
chiarafois2003 <foischiara20@gmail.com>
2026-04-15 11:43:54 +0200

    add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 
+	import "$lib/styles/reset.css";
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi Font -->
+	<link rel="preconnect" href="https://api.fontshare.com" />
+	<link rel="preconnect" href="https://api.fontshare.com" crossorigin>
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet">
+
+	<!-- Inter Font -->
+	<link rel="preconnect" href="https://fonts.bunny.net/" />
+	<link rel="preconnect" href="https://fonts.bunny.net/" crossorigin>
+	<link href="https://fonts.bunny.net/css2?family=Bowlby+One+SC&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Orbitron:wght@400..900&family=Staatliches&display=swap" rel="stylesheet">
+
 </svelte:head>
 
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,13 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
 	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[\\/]/)).includes('node_modules') ? undefined : true
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
 		adapter: adapter({
