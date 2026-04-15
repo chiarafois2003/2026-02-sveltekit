d7516b129429d8a261ee3329b9155c2556b1a425
chiarafois2003 <foischiara20@gmail.com>
2026-04-15 10:34:39 +0200

    configure mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
+	compilerOptions: {
+		runes: ({ filename }) => (filename.split(/[\\/]/)).includes('node_modules') ? undefined : true
+	},
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
