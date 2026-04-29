e65ea6b279af7140e115a1b8441a5dc9ae7c7773
chiarafois2003 <foischiara20@gmail.com>
2026-04-29 12:01:22 +0200

    Refactor svelte.config.js for improved readability and maintainability

--- svelte.config.js
+++ svelte.config.js
@@ -2,25 +2,23 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
-	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+    extensions: ['.svelte', '.md'],
+    preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'] })],
+    compilerOptions: {
+        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+    },
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        })
+        /* IL BLOCCO "paths" È STATO ELIMINATO! */
+    }
 };
 
 export default config;
